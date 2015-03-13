03481c44031752bd63d930d072450fa9cc2596f8
jkoba <jkoba0512@gmail.com>
2015-03-13 12:52:51 +0900

    add codes for motor drive

--- NexusTruckRobotEncoderTest.ino
+++ NexusTruckRobotEncoderTest.ino
@@ -3,6 +3,10 @@
 // Pin 6, 7: encoder for motor 2
 
 #include <PinChangeInt.h>
+const int pinMotor1PWM = 3;
+const int pinMotor1Dir = 2;
+const int pinMotor2PWM = 11;
+const int pinMotor2Dir = 12;
 
 const int pinEncoder1A = 4;
 const int pinEncoder1B = 5;
@@ -13,20 +17,44 @@ uint32_t counterEnc1 = 0;
 uint32_t counterEnc2 = 0;
 
 void setup() {
-//  pinMode(pinEncoder1A, INPUT_PULLUP);
+  // change PWM freq
+  TCCR1B=TCCR1B&0xf8|0x01;  // Pin9,Pin10 PWM 31250Hz
+  TCCR2B=TCCR2B&0xf8|0x01;  // Pin3,Pin11 PWM 31250Hz
+  
+  pinMode(pinMotor1PWM, OUTPUT);
+  pinMode(pinMotor1Dir, OUTPUT);
+  pinMode(pinMotor2PWM, OUTPUT);
+  pinMode(pinMotor2Dir, OUTPUT);
+
   pinMode(pinEncoder1A, INPUT);
   pinMode(pinEncoder1B, INPUT);
   PCintPort::attachPinChangeInterrupt(pinEncoder1A, riseEnc1A, RISING);
+  
   Serial.begin(115200);
 }
 
 void loop() {
+  // motor1
+  digitalWrite(pinMotor1Dir, HIGH);
+  analogWrite(pinMotor1PWM, 200);
+
+  // motor2
+  digitalWrite(pinMotor2Dir, HIGH);
+  analogWrite(pinMotor2PWM, 200);
+
+  Serial.println(counterEnc1);
+  
+  counterEnc1 = 0;
+  
+  delay(1000);
 }
 
 void riseEnc1A() {
   if (digitalRead(pinEncoder1A) == HIGH) {
+    counterEnc1++;
   }
   else {
+    counterEnc1--;
   }
 }
 
