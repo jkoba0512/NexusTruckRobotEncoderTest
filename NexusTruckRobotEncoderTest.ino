467bec85c7b29614e79c689adb9e26382f7ab96f
jkoba <jkoba0512@gmail.com>
2015-03-17 14:26:25 +0900

    introduced arrays

--- NexusTruckRobotEncoderTest.ino
+++ NexusTruckRobotEncoderTest.ino
@@ -7,31 +7,31 @@
 // Pin 6, 7: encoder for motor 2
 
 #include <PinChangeInt.h>
-const int pinMotor1PWM = 3;
-const int pinMotor1Dir = 2;
-const int pinMotor2PWM = 11;
-const int pinMotor2Dir = 12;
+
+typedef enum {CW, CCW} MotorDir;
+
+const int pinMotorPWM[3] = {100, 3, 11};
+const int pinMotorDir[3] = {100, 2, 12};
 
 const int pinEncoder1A = 4;
 const int pinEncoder1B = 5;
 const int pinEncoder2A = 6;
 const int pinEncoder2B = 7;
 
-uint32_t counterEnc1 = 0;
-uint32_t counterEnc2 = 0;
+int counterEnc[3] = {0, 0, 0};
 
-boolean clockwiseMotor1 = true;
-boolean clockwiseMoter2 = true;
+MotorDir motorDir[3] = {CW, CW, CW};
+int motorPWM[3] = {0, 0, 0}; 
 
 void setup() {
   // change PWM freq
   TCCR1B=TCCR1B&0xf8|0x01;  // Pin9,Pin10 PWM 31250Hz
   TCCR2B=TCCR2B&0xf8|0x01;  // Pin3,Pin11 PWM 31250Hz
   
-  pinMode(pinMotor1PWM, OUTPUT);
-  pinMode(pinMotor1Dir, OUTPUT);
-  pinMode(pinMotor2PWM, OUTPUT);
-  pinMode(pinMotor2Dir, OUTPUT);
+  for (int i = 1; i < 3; i++) {
+    pinMode(pinMotorPWM[i], OUTPUT);
+    pinMode(pinMotorDir[i], OUTPUT);
+  }
 
   pinMode(pinEncoder1A, INPUT);
   pinMode(pinEncoder1B, INPUT);
@@ -43,38 +43,29 @@ void setup() {
 
 void loop() {
   // motor1
-  digitalWrite(pinMotor1Dir, HIGH);
-  analogWrite(pinMotor1PWM, 200);
+  digitalWrite(pinMotorDir[1], HIGH);
+  analogWrite(pinMotorPWM[1], 200);
 
   // motor2
-  digitalWrite(pinMotor2Dir, HIGH);
-  analogWrite(pinMotor2PWM, 200);
+  digitalWrite(pinMotorDir[2], HIGH);
+  analogWrite(pinMotorPWM[2], 200);
 
-  Serial.print(counterEnc1);
+  Serial.print(counterEnc[1]);
   Serial.print(", ");
-  Serial.println(counterEnc2);
+  Serial.println(counterEnc[2]);
   
-//  counterEnc1 = 0;
-//  counterEnc2 = 0;
+  counterEnc[1] = counterEnc[2] = 0;
   
   delay(1000);
 }
 
 void riseEnc1A() {
-  if (digitalRead(pinEncoder1B) == HIGH) {
-    counterEnc1++;
-  }
-  else {
-    counterEnc1--;
-  }
+  if (digitalRead(pinEncoder1B) == HIGH) counterEnc[1]++;
+  else counterEnc[1]--;
 }
 
 void riseEnc2A() {
-  if (digitalRead(pinEncoder2B) == HIGH) {
-    counterEnc2++;
-  }
-  else {
-    counterEnc2--;
-  }
+  if (digitalRead(pinEncoder2B) == HIGH) counterEnc[2]++;
+  else counterEnc[2]--;
 }
 
